74d2aa17a0713389b0b7935072eb45e284cb5f36
ljeronimodarocha <ljeronimodarocha@gmail.com>
2023-03-22 20:21:07 -0300

    finished

---
+++ src/app/shared/poker-header/poker-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poke-header',
+  templateUrl: './poker-header.component.html',
+  styleUrls: ['./poker-header.component.scss']
+})
+export class PokerHeaderComponent {
+
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokerHeaderComponent } from './poker-header/poker-header.component';
+import { PokeSearchComponent } from './poke-search/poke-search.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    PokerHeaderComponent,
+    PokeSearchComponent,
+    PokeListComponent
+  ],
+  exports: [
+    PokerHeaderComponent,
+    PokeSearchComponent,
+    PokeListComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+  
+})
+export class SharedModule { }
